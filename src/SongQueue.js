d660646d7a57f9df8e7bdcf328a6c47742925cab
Yonet <aysegulyonet@gmail.com>
2013-08-21 23:22:59 -0700

    songqueue on ends function plays the first song left in the queque.

--- src/SongQueue.js
+++ src/SongQueue.js
@@ -1,12 +1,12 @@
 var SongQueue = Songs.extend({
 
   initialize: function(){
-    this.on('add', function(song){
+    this.on('add', function(){
       this.length === 1 && this.playFirst();
     }, this);
     this.on('ended', function(){
   	  this.remove(this.at(0));
-  	  this.length === 0 || this.playFirst;
+  	  this.length === 0 || this.playFirst(this.at(0));
   }, this);
   },
   playFirst: function(){
